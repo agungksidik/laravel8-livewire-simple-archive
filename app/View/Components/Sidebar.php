809a586e78ff1c4dec2e7ceaac9c0c0f7442c3bd
agungksidik <agungksidik@gmail.com>
2021-06-09 11:21:51 +0700

    add component

---
+++ app/View/Components/Sidebar.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+use Spatie\Permission\Models\Role;
+
+class Sidebar extends Component
+{
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\Contracts\View\View|\Closure|string
+     */
+    public function render()
+    {
+        $roles = Role::get();
+        return view('components.sidebar', compact('roles'));        
+    }
+}
