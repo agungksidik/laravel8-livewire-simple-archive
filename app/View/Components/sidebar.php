da2b4ff65f5879632c55f587daaca80a36521518
agungksidik <agungksidik@gmail.com>
2021-06-09 08:12:16 +0700

    uppercase component

--- app/View/Components/sidebar.php
+++ app/View/Components/sidebar.php
@@ -5,7 +5,7 @@ namespace App\View\Components;
 use Illuminate\View\Component;
 use Spatie\Permission\Models\Role;
 
-class sidebar extends Component
+class Sidebar extends Component
 {
     /**
      * Create a new component instance.
